c9118da66738ad2f3e38fdd64f7489fc48e00a1b
unknown <marvindinneweth@gmail.com>
2025-05-24 10:31:23 +0200

    update PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'vulture-cache-v1';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'logo.png',
+  'app-1.html',
+  'app-2.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
