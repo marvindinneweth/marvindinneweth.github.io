99c591923bf00eb32650f57416112f5413cd7298
Marvin Dinneweth <marvindinneweth@gmail.com>
2026-06-06 13:35:38 +0200

    working version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vulture-cache-v1';
+const CACHE_NAME = 'vulture-cache-v21';
 const ASSETS_TO_CACHE = [
   'index.html',
   'logo.png',
@@ -13,6 +13,20 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
